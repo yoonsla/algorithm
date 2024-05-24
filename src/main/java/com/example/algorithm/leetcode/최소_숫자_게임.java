db92156ac6e7c538eed70a69791c9199bc9e47b6
yoonsla <sangyoon5_4@naver.com>
2024-05-24 16:42:11 +0900

    최소 숫자 게임

---
+++ src/main/java/com/example/algorithm/leetcode/최소_숫자_게임.java
@@ -0,0 +1,48 @@
+package com.example.algorithm.leetcode;
+
+import java.util.Arrays;
+import java.util.PriorityQueue;
+import java.util.Queue;
+import java.util.stream.Collectors;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class 최소_숫자_게임 implements ApplicationRunner {
+
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+
+        Solution solution = new Solution();
+
+        int[] numbers = {5, 4, 2, 3}; // 3,2,5,4
+        int[] numbers2 = {2, 5}; // 5, 2
+        int[] answer = solution.numberGame(numbers);
+        for (int i : answer) {
+            System.out.println(i);
+        }
+    }
+
+    static class Solution {
+
+        public int[] numberGame(int[] nums) {
+            Queue<Integer> priorityQueue = Arrays.stream(nums)
+                .boxed()
+                .collect(Collectors.toCollection(PriorityQueue::new));
+            int[] answer = new int[nums.length];
+            int i = 0;
+            while (!priorityQueue.isEmpty()) {
+                final Integer first = priorityQueue.poll();
+                final Integer second = priorityQueue.poll();
+                if (second != null) {
+                    answer[i++] = second;
+                }
+                if (first != null) {
+                    answer[i++] = first;
+                }
+            }
+            return answer;
+        }
+    }
+}
