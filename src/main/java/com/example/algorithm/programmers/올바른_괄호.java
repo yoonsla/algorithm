a4b4742eb21310692b3480b418040caa731f00ec
yoonsla <sangyoon5_4@naver.com>
2024-05-24 16:42:11 +0900

    최소 숫자 게임

--- src/main/java/com/example/algorithm/programmers/올바른_괄호.java
+++ src/main/java/com/example/algorithm/programmers/올바른_괄호.java
@@ -4,7 +4,7 @@ import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Component;
 
-@Component
+//@Component
 public class 올바른_괄호 implements ApplicationRunner {
 
     @Override
