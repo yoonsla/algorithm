7cc23846cb86846d82521553628e904abdebbaa3
yoonsla <sangyoon5_4@naver.com>
2024-05-27 11:49:20 +0900

    배열의_두_요소의_최대_곱 #2

--- src/test/java/com/example/algorithm/MainTests.java
+++ src/test/java/com/example/algorithm/MainTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class AlgorithmApplicationTests {
+class MainTests {
 
     @Test
     void contextLoads() {
