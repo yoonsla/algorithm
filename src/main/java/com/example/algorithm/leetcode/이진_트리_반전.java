e4a4eeae1360b7f31d07869b0527dc952d574334
yoonsla <sangyoon5_4@naver.com>
2024-06-03 17:52:43 +0900

    이진_트리_반전

--- src/main/java/com/example/algorithm/leetcode/이진_트리_반전.java
+++ src/main/java/com/example/algorithm/leetcode/이진_트리_반전.java
@@ -4,35 +4,38 @@ import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Component;
 
-//@Component
-public class 이진_트리의_최대_깊이 implements ApplicationRunner {
+@Component
+public class 이진_트리_반전 implements ApplicationRunner {
 
     @Override
     public void run(ApplicationArguments args) throws Exception {
 
-        TreeNode root = new TreeNode(3);
-        root.left = new TreeNode(9);
-        root.right = new TreeNode(20);
-        root.right.left = new TreeNode(15);
-        root.right.right = new TreeNode(7);
-
-        TreeNode root2 = new TreeNode(1);
-        root2.right = new TreeNode(2);
+        // [4,2,7,1,3,6,9] ---> [4,7,2,9,6,3,1]
+        TreeNode root = new TreeNode(4);
+        root.left = new TreeNode(2);
+        root.right = new TreeNode(7);
+        root.left.left = new TreeNode(1);
+        root.left.right = new TreeNode(3);
+        root.right.left = new TreeNode(6);
+        root.right.right = new TreeNode(9);
 
         Solution solution = new Solution();
-        int result = solution.maxDepth(root);
+        TreeNode result = solution.invertTree(root);
         System.out.println(result);
     }
 
     class Solution {
 
-        public int maxDepth(TreeNode root) {
+        public TreeNode invertTree(TreeNode root) {
             if (root == null) {
-                return 0;
+                return null;
             }
-            int leftDepth = maxDepth(root.left);
-            int rightDepth = maxDepth(root.right);
-            return Math.max(leftDepth, rightDepth) + 1;
+            TreeNode leftTreeNode = root.left;
+            root.left = root.right;
+            root.right = leftTreeNode;
+            invertTree(root.left);
+            invertTree(root.right);
+            return root;
         }
     }
 
