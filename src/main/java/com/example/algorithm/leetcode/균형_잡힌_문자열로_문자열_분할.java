b30876412b852edce99d5bee172b8f1c4e31bfc6
yoonsla <sangyoon5_4@naver.com>
2024-06-05 12:13:31 +0900

    균형_잡힌_문자열로_문자열_분할

---
+++ src/main/java/com/example/algorithm/leetcode/균형_잡힌_문자열로_문자열_분할.java
@@ -0,0 +1,39 @@
+package com.example.algorithm.leetcode;
+
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.stereotype.Component;
+
+//@Component
+public class 균형_잡힌_문자열로_문자열_분할 implements ApplicationRunner {
+
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+
+        String text1 = "RLRRLLRLRL"; // 4
+        String text2 = "RLRRRLLRLL"; // 2
+        String text3 = "LLLLRRRR";   // 1
+
+        Solution solution = new Solution();
+        int result = solution.balancedStringSplit(text1);
+        System.out.println(result);
+    }
+
+    class Solution {
+        public int balancedStringSplit(String text) {
+            int count = 0;
+            int result = 0;
+            for (int i = 0; i < text.length(); i++) {
+                if (text.charAt(i) == 'R') {
+                    count++;
+                } else {
+                    count--;
+                }
+                if (count == 0) {
+                    result++;
+                }
+            }
+            return result;
+        }
+    }
+}
