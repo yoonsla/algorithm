161d0bf038054f4f1d6f87394d4f3be9aba6edb9
yoonsla <sangyoon5_4@naver.com>
2024-06-14 16:00:44 +0900

    기수정렬 md 파일

---
+++ src/main/java/com/example/algorithm/document/기수정렬.java
@@ -0,0 +1,14 @@
+package com.example.algorithm.document;
+
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class 기수정렬 implements ApplicationRunner {
+
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+
+    }
+}
