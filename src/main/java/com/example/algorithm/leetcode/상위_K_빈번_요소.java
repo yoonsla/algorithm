f24045705d6bfcdf750c6afa8e1354d183aa8758
yoonsla <sangyoon5_4@naver.com>
2024-06-20 16:34:50 +0900

    상위_K_빈번_요소

---
+++ src/main/java/com/example/algorithm/leetcode/상위_K_빈번_요소.java
@@ -0,0 +1,41 @@
+package com.example.algorithm.leetcode;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Map.Entry;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class 상위_K_빈번_요소 implements ApplicationRunner {
+
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+
+        // 2
+        int[] nums = {1, 1, 1, 2, 2, 3};
+        int k = 2;
+
+        Solution solution = new Solution();
+        int[] result = solution.topKFrequent(nums, k);
+        System.out.println("result: " + Arrays.toString(result));
+    }
+
+    static class Solution {
+
+        public int[] topKFrequent(int[] numbers, int k) {
+            Map<Integer, Integer> map = new HashMap<>();
+
+            for (int number : numbers) {
+                map.put(number, map.getOrDefault(number, 0) + 1);
+            }
+            return map.entrySet().stream()
+                .sorted((e1, e2) -> e2.getValue().compareTo(e1.getValue()))
+                .limit(k)
+                .mapToInt(Entry::getKey)
+                .toArray();
+        }
+    }
+}
